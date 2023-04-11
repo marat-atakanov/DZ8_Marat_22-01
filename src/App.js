159d7b0a20c561d5247ef44808f0edc684416e2a
marat-atakanov <marat.kg02@gmail.com>
2023-04-11 15:49:05 +0600

    dz8

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import {useForm} from "react-hook-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// export default function App(){
+//   const {register, handleSubmit, watch, formState: {errors}} = useForm()
+//   const onSubmit = data => console.log(data)
+//
+//   console.log(watch("example"));
+//   return (
+//       <form onSubmit={handleSubmit(onSubmit)}>
+//         <input defaultValue="test" {...register("example")}/>
+//         <input {...register("exampleRequired", {required: true})}/>
+//         {errors.exampleRequired && <span>This field is required</span>}
+//         <input type="submit"/>
+//       </form>
+//   )
+// }
+
+export default function App() {
+    const {register, handleSubmit, formState: {errors}} = useForm()
+    const onSubmit = data => {
+        console.log(data.birthDate);
+        alert(data.firstName + " " + data.lastName + " " + data.gender + " " + (data.birthDate !== "" ? data.birthDate : "Not indicated"))
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="container">
+                <p className="title">Registration form</p>
+                <div className="firstLastNames">
+                    <input {...register("firstName", {required: true})} placeholder="First name"/>
+                    <input {...register("lastName", {required: true})} placeholder="Last name"/>
+                </div>
+                <input className="dateInput" {...register("birthDate", {required: false})} type="date"/>
+                <select {...register("gender")}>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Other">Other</option>
+                </select>
+                <button className="submitButton" type="submit">submit</button>
+                {errors.firstName && <p className="firstNameError">First name is required </p>}
+                {errors.lastName && <p className="lastNameError">Last name is required </p>}
+            </div>
+        </form>
+    )
 }
-
-export default App;
